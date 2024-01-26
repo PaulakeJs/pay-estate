22fc4539411a97167b27c780a53dd0d03ce7129e
paulakejs <developerpaulake@gmail.com>
2024-01-26 12:08:58 +0100

    fou

--- apis/server.js
+++ apis/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const cors = require("cors");
 
 const path = require("path");
-const __dirname = path.resolve();
-
 const app = express();
 const bcrypt = require("bcryptjs");
 require("dotenv").config();
@@ -11,19 +9,21 @@ const db = require("./config/db");
 const User = require("./models/usersModel");
 const usersRoutes = require("./Routes/usersRoutes");
 const listingsRoutes = require("./Routes/listingsRoute");
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", usersRoutes);
 app.use("/api/listing", listingsRoutes);
 
+// Use __dirname within the module scope
+const currentDir = __dirname;
 
-app.use(express.static(path.join(__dirname,'client/dist')))
+app.use(express.static(path.join(currentDir, 'client/dist')));
 
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname , 'client', 'dist' , 'index.html'))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(currentDir, 'client', 'dist', 'index.html'));
+});
 
 const PORT = 7000;
 app.listen(PORT, () => {
